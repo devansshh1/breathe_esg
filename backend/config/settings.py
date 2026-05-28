79267dd0f68b6617bd37ae364c7c45eb68481849
devansshh1 <devanshpersonalll@gmail.com>
2026-05-28 19:10:34 +0530

    working successfully v1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,7 +2,8 @@ import os
 from datetime import timedelta
 from pathlib import Path
 import dj_database_url
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
